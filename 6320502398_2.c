2f0f59a591cbdcfc8e2b6c20d8b7493af8e60545
ntwww <nattawut.rod@ku.th>
2021-03-29 17:41:32 +0700

    2.2

--- 6320502398_2.c
+++ 6320502398_2.c
@@ -3,14 +3,14 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int i,number[10000];
-    for(i=0;i<n;i++)
+    int i,number[n];
+    for(i=0; i<n; i++)
     {
         scanf("%d",&number[i]);
     }
     int nub=0,max=number[0];
 
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         if(max<number[i])
         {
@@ -18,6 +18,34 @@ int main()
             nub=i+1;
         }
     }
-    printf("%d %d",max,nub);
-
+    int check=1,nub_max=0;
+    for(i=0; i<n; i++)
+    {
+        if(number[i]==max)
+        {
+            check=0;
+            nub_max++;
+        }
+    }
+    if(nub_max==1)
+    {
+        check=1;
+    }
+    int min=number[0],nub_min;;
+    if(check==1)
+    {
+        printf("%d",nub);
+    }
+    else
+    {
+        for(i=0; i<n; i++)
+        {
+            if(min>number[i])
+            {
+                min=number[i];
+                nub_min=i+1;
+            }
+        }
+        printf("%d %d",min,max);
+    }
 }
